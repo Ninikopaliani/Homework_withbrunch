4fa80eb85bd4cf49af6c5d8c7c0c4d6500acd667
Ninikopaliani <kopalianini@gmail.com>
2024-03-03 22:02:23 +0400

    second homework

---
+++ Homework_2/Task_3.py
@@ -0,0 +1,13 @@
+number_1 = int(input("შეიყვანე რიცხვი 1-დან 10-ის ჩათვლით: "))
+a = number_1 % 2
+b = number_1 % 3
+c = number_1 % 5
+if number_1 <= 10:
+    if a == 0 and b == 0:
+        print(number_1//2, ",", number_1//3)
+    elif a == 0 and c == 0:
+        print(number_1//2, ",", number_1//5)
+    else:
+        print("რიცხვს აქვს 2ზე ნაკლები მარტივი გამყოფი , გარდა თავის თავისა და 1-ის")
+else:
+    print("გთხოვთ შეიყვანეთ, 1-დან 10-ის ჩათვლით რიცხვი")
