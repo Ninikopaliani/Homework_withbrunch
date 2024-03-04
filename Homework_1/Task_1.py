1852d7a8fb8aa9733151fb54f748bf047706f673
Ninikopaliani <kopalianini@gmail.com>
2024-03-04 23:15:50 +0400

    fixed by comment task1

--- Homework_1/Task_1.py
+++ Homework_1/Task_1.py
@@ -1,4 +1,4 @@
 user_name = input("enter your name: ")
-User_lastname = input("enter your last name: ")
+user_lastname = input("enter your last name: ")
 user_age = input("enter how old are you: ")
-print("გამარჯობა,",user_name,User_lastname,"თქვენ ხართ",user_age,"წლის")
+print("გამარჯობა,", user_name, user_lastname, "თქვენ ხართ", user_age, "წლის")
