a73adfdd349b4d3b1be4ed96cb611dfc37434d46
Ninikopaliani <kopalianini@gmail.com>
2024-03-12 00:34:03 +0400

    fixd task2 from homework5

--- homework_5/Task _2.py
+++ homework_5/Task _2.py
@@ -1,7 +1,5 @@
-for a in range(1, 10):
-    for b in range(1, 10):
-        if b>1:
-            print(f"{a}*{b}={a*b}",end=" ")
-        print(f"{a+1}*{b}={(a+1)*b}")
-
-
+for i in range(1, 10):
+    for j in range(1, i + 1):
+        print(f"{j} * {i} = {i * j}", end=" ")
+    print()
+# while loop-is შემდეგ რატომღაც უფრო მივხვდი for -ის შინაარსს და გვიან ჩავასწორე მე-5 დავალება  :(
